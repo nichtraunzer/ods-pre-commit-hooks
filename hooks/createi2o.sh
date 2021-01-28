dd111c93bab06eef71b8839f844aabc336ad0074
tbugfinder <github@online.ms>
2021-01-28 15:48:40 +0100

    add hook for creating the output inputs2outputs. The output passes all input variables.

---
+++ hooks/createi2o.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Author: Josef Hartmann
+#         josef.hartmann@boehringer-ingelheim.com
+#
+# Dependencies:
+#                - terraform >= 0.12.x
+#                - terraform-docs
+#                - jq
+#
+# This scripts creates a inputs2outputs.tf file.
+# The file defines a terraform output inputs2outputs that passes module inputs (variables) as outputs.
+#
+
+
+set -e
+set -o pipefail
+
+indenttfo() { sed 's/^/          /'; }
+
+CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/../.." && pwd -P)"
+TFHFILE=${CWD}/inputs2outputs.tf
+
+if [ -f "${TFHFILE}" ] ; then
+    rm -f "${TFHFILE}"
+fi
+
+TFDOCSHCL=$(terraform-docs tfvars json "${CWD}" | jq -r 'keys[] as $k | "\($k) = var.\($k)"' | indenttfo )
+
+cat > "${TFHFILE}" <<EOF
+output "inputs2outputs" {
+  description = "all inputs passed to outputs"
+  value = [{
+${TFDOCSHCL}
+  }]
+}
+EOF
+terraform fmt "${TFHFILE}"
